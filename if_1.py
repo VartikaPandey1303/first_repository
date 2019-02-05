346bbb7defc2fedd012f51fc35bf594fe5e114d3
VartikaPandey1303 <45865677+VartikaPandey1303@users.noreply.github.com>
2019-02-06 01:58:07 +0530

    Add files via upload

---
+++ if_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 17:03:41 2019
+
+@author: user
+"""
+
+#grades to be printed
+day=int(input('enter a digit between 1 to 7 '))
+if (day==1):
+    print('It is monday')
+elif (day==2):
+    print('It is tuesday')    
+elif (day==3):
+    print('It is wednesday') 
+elif (day==4):
+    print('It is thursday')   
+elif (day==5):
+    print('It is friday')    
+elif (day==6):
+    print('It is saturday')
+elif (day==7):
+    print('It is sunday')    
+else:
+    print('error')    
